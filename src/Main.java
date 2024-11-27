d630978ff5581bbe5c34b4d9ea9bb9c9ce2e1acb
Anatoly Samoylov <9295730700@mail.ru>
2024-11-27 22:14:57 +0300

    Изменили функционал приложения, добавлены папки "src", "META-INF с файлом MANIFEST.MF"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
+import static java.awt.SystemColor.text;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите текст и нажмите <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
+
     }
 }
