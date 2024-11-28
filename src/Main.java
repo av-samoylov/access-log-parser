5f58d6f0fe3dc4903e65d6db99596dfd6a4d86ae
Anatoly Samoylov <9295730700@mail.ru>
2024-11-28 15:25:56 +0300

    Изменил метод maim, добавлена функция вычисления двух вводимых чисел в консоль операторами */+-

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,21 @@ import static java.awt.SystemColor.text;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
 
+        int product = firstNumber * secondNumber;
+        System.out.println("Произведение: " + product);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма: " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность: " + difference);
     }
 }
