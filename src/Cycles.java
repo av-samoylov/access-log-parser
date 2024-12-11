b599ed1c4204411c13fbd175c1492442775b19ec
Anatoly Samoylov <9295730700@mail.ru>
2024-12-11 15:22:35 +0300

    Добавлен класс "Cycles.java" в котором реализавана функциональность парсера лог-файлов

---
+++ src/Cycles.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Cycles {
+    public static void main(String[] args) {
+        int validFileCount = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Это не файл, а директория");
+                continue;
+            } else {
+                validFileCount++;
+                System.out.println("Путь указан верно. Это файл номер N" + validFileCount);
+            }
+        }
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int product = firstNumber * secondNumber;
+        System.out.println("Произведение: " + product);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма: " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность: " + difference);
     }
+
+    public static double fraction(double x) {
+        return 0;
+    }
+
 }
